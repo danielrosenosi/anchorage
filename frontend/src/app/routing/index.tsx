f3e9751c657c8ae26b6ec66b1610d02cc7cb006a
devroseno <devroseno@gmail.com>
2023-05-28 15:01:58 -0300

    feat: retrieving url id

--- frontend/src/app/routing/index.tsx
+++ frontend/src/app/routing/index.tsx
@@ -5,12 +5,14 @@ import {
 } from "react-router-dom";
 
 import { Home } from "../../pages/Home";
+import { Attendance } from "../../pages/Attendance";
 
 export function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path ="/attendance/:id" element={<Attendance />} />
             </Routes>
         </BrowserRouter>
     );
