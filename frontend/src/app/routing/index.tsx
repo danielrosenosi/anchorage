97fb07481707122e85bbb13bea7aac94e042db84
devroseno <devroseno@gmail.com>
2023-07-08 23:48:37 -0300

    feat: new pages

--- frontend/src/app/routing/index.tsx
+++ frontend/src/app/routing/index.tsx
@@ -4,16 +4,20 @@ import {
     Route
 } from "react-router-dom";
 
-import { Home } from "../../pages/Home";
-import { Attendance } from "../../pages/Attendance";
+import { Patients } from "../../pages/Patients";
+import { Users } from "../../pages/Users";
 import { Sidebar } from "../components/Sidebar";
+import { Dashboard } from "../../pages/Dashboard";
+import { Attendance } from "../../pages/Attendance";
 
 export function AppRoutes() {
     return (
         <BrowserRouter>
             <Sidebar>
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/usuarios" element={<Users />} />
+                    <Route path="/pacientes" element={<Patients />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
                     <Route path ="/attendance/:id" element={<Attendance />} />
                 </Routes>
             </Sidebar>
